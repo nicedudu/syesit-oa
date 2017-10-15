4a36dcdd9d2ef66c9e149d5f1cc2be0953b903ed
Ring <www.592026320@qq.com>
2017-10-15 23:24:35 +0800

    add middleware

--- src/common/middlewares/auth.middleware.ts
+++ src/common/middlewares/auth.middleware.ts
@@ -1,21 +1,24 @@
-import { Middleware, NestMiddleware } from '@nestjs/common';
+import { Middleware, NestMiddleware, ExpressMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
-import { User, UserSercvice } from '../../entity/user/index';
+import { UserSercvice } from '../../entity/user/index';
 
 @Middleware()
 export class AuthMiddleware implements NestMiddleware {
-    constructor(private readonly userService:UserSercvice){}
-    public resolve() {
+    constructor(private readonly userService: UserSercvice) { }
+    public resolve(): ExpressMiddleware {
         return async (req: Request, res: Response, next: NextFunction) => {
             if (req.headers.authorization && (req.headers.authorization as string).split(' ')[0] === 'Bearer') {
                 const token = (req.headers.authorization as string).split(' ')[1];
-                const decoded:any = jwt.verift(token,process.env.JWT_KEY || '');
+                const decoded: any = jwt.verify(token, process.env.JWT_KEY || '');
                 const user = await this.userService.get(decoded.id);
-                if(!user) console.log('非法用户')
+                if (!user) console.log('非法用户')
                 next();
-            }else{
-                console.log('用户尚未登录')
+            } else {
+                console.log('用户尚未登录');
+                const user = await this.userService.get(1);
+                console.log(user);
+                next();
             }
         }
     }
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module , NestModule , MiddlewaresConsumer , RequestMethod} from '@nestjs/common';
 import { GraphqlController } from '../controller/graphql.controller';
 import { AuthModule } from '../common/auth/auth.module';
+import { AuthMiddleware } from '../common/middlewares/auth.middleware';
 
 @Module({
     controllers: [GraphqlController],
     modules: [AuthModule],
 })
-export class ApplicationModule { }
+export class ApplicationModule implements NestModule { 
+    configure(consumer:MiddlewaresConsumer):void{
+        consumer.apply(AuthMiddleware).forRoutes({
+            path:'/auth/login',method:RequestMethod.GET
+        })
+    }
+}
